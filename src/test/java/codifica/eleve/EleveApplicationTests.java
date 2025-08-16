aea0682692579c19fa7f895a0b4f55d4efa709a1
Felippe-ASLopes <felippedias3@gmail.com>
2025-08-16 15:43:17 -0300

    :tada: init: clean architecture

---
+++ src/test/java/codifica/eleve/EleveApplicationTests.java
@@ -0,0 +1,13 @@
+package codifica.eleve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
