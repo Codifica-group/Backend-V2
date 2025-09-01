8280e80a124d0a9935696730ea2092d4bbf0a522
Felippe-ASLopes <felippedias3@gmail.com>
2025-09-01 18:52:13 -0300

    :sparkles: feat: liberando CORS para integração com front

--- src/main/java/codifica/eleve/config/SecurityConfig.java
+++ src/main/java/codifica/eleve/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Permite todas as requisições OPTIONS
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/usuarios/login").permitAll()
                         .anyRequest().authenticated()
