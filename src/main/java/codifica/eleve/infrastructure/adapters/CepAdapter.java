aa7ec5cb0b0dbe89e75f04304bfc39225862b94c
Felippe-ASLopes <felippedias3@gmail.com>
2025-11-16 12:27:48 -0300

    :sparkles: feat: tratativa de erro para API Busca CEP

--- src/main/java/codifica/eleve/infrastructure/adapters/CepAdapter.java
+++ src/main/java/codifica/eleve/infrastructure/adapters/CepAdapter.java
@@ -4,9 +4,11 @@ import codifica.eleve.core.application.ports.out.CepPort;
 import codifica.eleve.core.domain.shared.Endereco;
 import codifica.eleve.core.domain.shared.exceptions.InternalServerErrorException;
 import codifica.eleve.core.domain.shared.exceptions.NotFoundException;
+import codifica.eleve.core.domain.shared.exceptions.IllegalArgumentException;
 import codifica.eleve.interfaces.dto.CepDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -34,6 +36,14 @@ public class CepAdapter implements CepPort {
                     cepDTO.getComplemento()
             );
 
+        }catch (NotFoundException e) {
+            throw e;
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new NotFoundException("CEP não encontrado: " + cep);
+        } catch (HttpClientErrorException.BadRequest e) {
+            throw new IllegalArgumentException("Formato de CEP inválido: " + cep);
         } catch (Exception e) {
             throw new InternalServerErrorException("Erro interno na API VIACEP: " + e.getMessage());
         }
